e45e7e942ae91f2094efb16dc7373dd58fb4df99
Casey Ochieng <caseyochieng46@gmail.com>
2023-03-19 00:50:10 +0300

    completed

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,7 +1,6 @@
 class ToysController < ApplicationController
   wrap_parameters format: []
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-
+  
   def index
     toys = Toy.all
     render json: toys
@@ -13,29 +12,21 @@ class ToysController < ApplicationController
   end
 
   def update
-    toy = find_by
-    toy.update(likes: toy.likes + 1)
+    toy = Toy.find_by(id: params[:id])
+    toy.update(toy_params)
     render json: toy
   end
 
   def destroy
-    toy = find_by
+    toy = Toy.find_by(id: params[:id])
     toy.destroy
     head :no_content
   end
 
   private
-
-
-  def find_by
-    Toy.find_by(id: params[:id])
-  end
   
   def toy_params
     params.permit(:name, :image, :likes)
   end
 
-  def render_not_found_response
-    render json: { error: "Bird not found" }, status: :not_found
-  end
 end
